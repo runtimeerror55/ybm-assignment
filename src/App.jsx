85f5f49e98f1d57ea21fb5e53cd532ea60b74cc8
Aakash <runtimeerror55@gmail.com>
2024-11-05 14:42:36 +0530

    feat: close,open post functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,44 @@
+import { useState } from "react";
 import "./App.css";
 import TwoArrowsInOppositeDirectionIcon from "./assets/icons/twoArrowsInOppositeDirectionIcon";
 import thread from "./threads";
 function App() {
+      const [showExpandIcon, setShowExpandIcon] = useState(true);
       return (
             <div>
                   {thread.map((item, index) => {
                         return (
                               <div key={index}>
                                     <div className="flex gap-2 items-center">
-                                          <TwoArrowsInOppositeDirectionIcon className="text-blue-500 w-3 h-3"></TwoArrowsInOppositeDirectionIcon>
+                                          {showExpandIcon ? (
+                                                <TwoArrowsInOppositeDirectionIcon
+                                                      className="text-blue-500 w-3 h-3"
+                                                      onClick={() => {
+                                                            setShowExpandIcon(
+                                                                  false
+                                                            );
+                                                      }}
+                                                ></TwoArrowsInOppositeDirectionIcon>
+                                          ) : null}
                                           <img
                                                 src={item.author.avatar}
                                                 className="h-10"
                                           ></img>
                                           <div>{item.author.name}</div>
                                     </div>
-                                    <div className="flex gap-3 mt-3">
-                                          <div className="w-[0.12rem] bg-gray-300 hover:bg-blue-500 cursor-pointer"></div>
-                                          <div>{item.text}</div>
-                                    </div>
+                                    {!showExpandIcon ? (
+                                          <div className="flex gap-3 mt-3 ml-5">
+                                                <div
+                                                      className="w-[0.12rem] bg-gray-300 hover:bg-blue-500 cursor-pointer"
+                                                      onClick={() => {
+                                                            setShowExpandIcon(
+                                                                  true
+                                                            );
+                                                      }}
+                                                ></div>
+                                                <div>{item.text}</div>
+                                          </div>
+                                    ) : null}
                               </div>
                         );
                   })}
