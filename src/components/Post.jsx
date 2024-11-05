b1b9cb918886a460a8ca8ca68a1904fd267440c9
Aakash <runtimeerror55@gmail.com>
2024-11-05 16:25:47 +0530

    feat: recursively render replies on expand icon click

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -16,6 +16,27 @@ const Post = ({ item }) => {
                         <img src={item.author.avatar} className="h-10"></img>
                         <div>{item.author.name}</div>
                   </div>
+
+                  <div
+                        className={`gap-3 mt-3 ml-5 ${
+                              !showExpandIcon ? "flex" : "hidden"
+                        }`}
+                  >
+                        <div
+                              className="w-[0.12rem] bg-gray-300 hover:bg-blue-500 cursor-pointer"
+                              onClick={() => {
+                                    setShowExpandIcon(true);
+                              }}
+                        ></div>
+                        <div className="flex flex-col gap-3">
+                              <div>{item.text}</div>
+                              {item.replies?.map((item, index) => {
+                                    return (
+                                          <Post key={index} item={item}></Post>
+                                    );
+                              })}
+                        </div>
+                  </div>
             </div>
       );
 };
