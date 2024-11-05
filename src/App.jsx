0f47ee036f578acdca16e10c0c1d7b3c48f23feb
Aakash <runtimeerror55@gmail.com>
2024-11-05 14:25:35 +0530

    feat: render post with basice styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
 import "./App.css";
-
+import TwoArrowsInOppositeDirectionIcon from "./assets/icons/twoArrowsInOppositeDirectionIcon";
+import thread from "./threads";
 function App() {
-      return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+      return (
+            <div>
+                  {thread.map((item, index) => {
+                        return (
+                              <div key={index}>
+                                    <div className="flex gap-2 items-center">
+                                          <TwoArrowsInOppositeDirectionIcon className="text-blue-500 w-3 h-3"></TwoArrowsInOppositeDirectionIcon>
+                                          <img
+                                                src={item.author.avatar}
+                                                className="h-10"
+                                          ></img>
+                                          <div>{item.author.name}</div>
+                                    </div>
+                                    <div className="flex gap-3 mt-3">
+                                          <div className="w-[0.12rem] bg-gray-300 hover:bg-blue-500 cursor-pointer"></div>
+                                          <div>{item.text}</div>
+                                    </div>
+                              </div>
+                        );
+                  })}
+            </div>
+      );
 }
 
 export default App;
